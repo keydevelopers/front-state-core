96e58217976e5f0451510b6b66bc31902ee16858
keydevelopers <sloppydeveloper@proton.me>
2024-03-21 02:29:56 -0400

    Revert "chore: reorganized files"
    This reverts commit 06f98617855f664b16c3ebd55bec2bbbce35d23d.

--- mod/globalContext.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from "./context.ts";
-
-export const globalContext: Context = {
-    states: new Set(),
-    registrations: new Set(),
-    cleanup: new Set(),
-    contexts: new Set(),
-    controller: new AbortController()
-};
